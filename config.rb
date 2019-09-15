05de25942e94f4af0f213c171b6bb2c3ecda69cc
nimmis <kjell.havneskold@gmail.com>
2019-09-15 17:07:55 +0000

    sample configuration file

---
+++ config.rb
@@ -0,0 +1,29 @@
+
+
+#
+# define number of nodes for the cluster
+# 1 = single node, acts as both master and nodes
+#
+#$num_nodes = 1
+
+#
+# name of masternode
+#
+#$master_name = "master"
+
+#
+# prefix of worker nodes
+# 
+#$node_prefix = "node-"
+
+#
+# traefik http port forwarding on host system
+# set $$http_port = 0 to disable forwarding
+#
+#$http_port = 8080
+
+#
+# traefik https port forwarding on host system
+# set $$https_port = 0 to disable forwarding
+#
+#$https_port = 8443
