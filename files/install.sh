b0ea3f717257458d3faad70f4808b56c816e42c3
nimmis <kjell.havneskold@gmail.com>
2019-09-29 08:24:54 +0200

    fixed default installation script to add iptables and fix missing rule

--- files/install.sh
+++ files/install.sh
@@ -4,10 +4,16 @@ cp -rp /tmp/user-data/copy/* /
 
     apk update
     apk  add -u  apk-tools
-    apk add curl
+    apk add curl iptables
 
     rc-update add cgroups boot
     rc-service cgroups start
+
+    rc-update add iptables
+    /etc/init.d/iptables save 
+    iptables -I INPUT 1 -i cni0 -s 10.42.0.0/16 -j ACCEPT
+    /etc/init.d/iptables save 
+    rc-service iptables start
 
     mkdir -p /home/vagrant/.ssh
     cat /tmp/user-data/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
