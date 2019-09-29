db0d0084df921b672140aaee3340f11ee0b5333d
nimmis <kjell.havneskold@gmail.com>
2019-09-29 08:08:52 +0200

    Fix host key error when deploying

--- get-config.sh
+++ get-config.sh
@@ -4,4 +4,4 @@ IP=$(vagrant ssh-config $1| grep HostName | awk '{print $2}')
 PORT=$(vagrant ssh-config $1| grep Port | awk '{print $2}')
 KEY=$(vagrant ssh-config $1| grep IdentityFile | awk '{print $2}')
 
-ssh -i $KEY -p $PORT vagrant@$IP "sudo -S cat /etc/rancher/k3s/k3s.yaml"
+ssh -o StrictHostKeyChecking=no -o GlobalKnownHostsFile=/dev/null -o UserKnownHostsFile=/dev/null  -i $KEY -p $PORT vagrant@$IP "sudo -S cat /etc/rancher/k3s/k3s.yaml"
