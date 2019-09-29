8827005f758ae574a07d397c4e564c6616d174f6
nimmis <kjell.havneskold@gmail.com>
2019-09-29 08:12:18 +0200

    Fix k3s install scripts on guest

--- files/agent.sh
+++ files/agent.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-K3S_TOKEN=$(sh -t -o "StrictHostKeyChecking no"  -i /tmp/user-data/id_rsa vagrant@192.168.50.10 "sudo cat /var/lib/rancher/k3s/server/node-token" 2> /dev/null)
+export TOKEN=$(ssh -t -o "StrictHostKeyChecking no"  -i /tmp/user-data/id_rsa vagrant@192.168.50.10 "sudo cat /var/lib/rancher/k3s/server/node-token" 2> /dev/null |
+        tr -d '\r' )
 
 echo "192.168.50.10 master" >> /etc/hosts
 
-curl -sfL https://get.k3s.io | K3S_URL=https://master:6443 K3S_TOKEN=$K3S_TOKEN sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.50.10:6443 --token $TOKEN" sh -s -
+
